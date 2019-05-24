e036c5fce77c40bbb8364e39891fa37511fa6abd
Flavio Henrique <flavioh825@gmail.com>
2019-05-23 23:04:40 -0300

    add code for coffee script

--- spec/system/exchanges_index_system_spec.rb
+++ spec/system/exchanges_index_system_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe "Exchange Currency Process", :type => :system, js: true do
     within("#exchange_form") do
       select('EUR', from: 'source_currency')
       select('USD', from: 'target_currency')
-      fill_in 'amount', with: '10'
+      fill_in 'amount', with: rand(1..9999)
     end
 
-    click_buttom 'CONVERTER'
+    click_button 'CONVERTER'
 
     # save_and_open_page
     expect(page).to have_content("value")  
