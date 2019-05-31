33b7f77a253a8eddd7911ab92a4f4a12b605a7ef
Flavio Henrique <flavioh825@gmail.com>
2019-05-31 00:06:45 -0300

    Cryptocoins controller implementation concluded

--- app/controllers/cryptocoins_controller.rb
+++ app/controllers/cryptocoins_controller.rb
@@ -1,4 +1,7 @@
 class CryptocoinsController < ApplicationController
   def convert
+    value = CryptocoinService.new(
+        params[:source_cryptocoin], params[:target_cryptocoin], params[:amount]).perform
+    render json: {"value": value}
   end
 end
