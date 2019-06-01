127420d30b4dd833da11b52e806e2b79204c72c5
Flavio Henrique <flavioh825@gmail.com>
2019-06-01 14:55:59 -0300

    Adjust cryptocoin service test

--- spec/services/cryptocoin_service_spec.rb
+++ spec/services/cryptocoin_service_spec.rb
@@ -5,7 +5,7 @@ require './app/services/cryptocoin_service'
 describe 'Cryptocoin' do
   it 'exchange' do
     amount = rand(0..9999)
-    res = CryptocoinService.new("BTC", "BRL", amount).perform
+    res = CryptocoinService.new("BRL", "BTC", amount).perform
     expect(res.is_a? Numeric).to eql(true)
     expect(res != 0 || amount == 0).to eql(true)
   end
---
+++ spec/support/cryptocoin_api_test.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:get, /rest.coinapi.io/)
+    .with(headers: {
+      'Accept' => '*/*'
+    }).to_return(status: 200, body: '
+      {
+        "time": "2019-08-11T14:31:18.3150000Z",
+        "asset_id_base": "BTC",
+        "asset_id_quote": "USD",
+        "rate": 3260.3514321215056208129867667
+      }', headers: {})
+  end
+end
